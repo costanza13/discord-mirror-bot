3c06cf687607b827c3ac879372fe0881830bbee1
mcc <costanza+git@gmail.com>
2021-06-10 17:07:05 -0700

    add polling with throttling for inactivity

--- public/examples/script.js
+++ public/examples/script.js
@@ -1,24 +1,27 @@
 const serverBaseUrl = "http://localhost:8000";
+const minPollInterval = 2;  // initial polling interval, will be adjusted based on activity
+let pollInterval = minPollInterval;
+let secondsSinceLastMessage = 0;
+let etag = '';
 
 const chatEl = document.querySelector('#chat');
 const loadingMsgEl = document.querySelector('#loading-msg');
-const messagesEl = document.createElement('ul');
-messagesEl.setAttribute('id', 'messages');
-chatEl.appendChild(messagesEl);
 
 
 const loadMessages = function () {
+  let headers;
   return fetch(serverBaseUrl + '/api/messages')
     .then(response => {
-      console.log(response);
+      headers = response.headers;
       return response.json();
     })
     .then(messages => {
-      console.log(messages);
       if (messages.length) {
+        etag = headers.get('etag');
         if (loadingMsgEl) {
           loadingMsgEl.remove();
         }
+        const messagesEl = document.createElement('ul');
         let lastHandle = '';
         for (let i = 0; i < messages.length; i++) {
           const messageEl = document.createElement('li');
@@ -30,6 +33,12 @@ const loadMessages = function () {
           messagesEl.appendChild(messageEl);
           lastHandle = messages[i].handle;
         }
+        const oldMessagesEl = document.querySelector('#messages');
+        if (oldMessagesEl) {
+          oldMessagesEl.remove();
+        }
+        messagesEl.setAttribute('id', 'messages');
+        chatEl.appendChild(messagesEl);
       }
       return messages.length;
     })
@@ -37,6 +46,31 @@ const loadMessages = function () {
 };
 
 // poll for updates with head request
+const pollForMessages = function () {
+  return fetch(serverBaseUrl + '/api/messages', { method: 'HEAD' })
+    .then(response => {
+      if (response.headers.get('etag') !== etag) {
+        secondsSinceLastMessage = 0;
+        pollInterval = minPollInterval;
+        loadMessages().then(len => {
+          chatEl.scrollTop = chatEl.scrollHeight;
+          etag = response.headers.get('etag');
+        });
+      } else {
+        secondsSinceLastMessage += pollInterval;
+        if (secondsSinceLastMessage > 60 * minPollInterval && pollInterval < 3 * minPollInterval) {
+          pollInterval += minPollInterval;
+        }
+      }
+      setTimeout(pollForMessages, pollInterval);
+    })
+    .catch(error => console.error(error));
+}
 
-
-loadMessages().then((len) => { chatEl.scrollTop = chatEl.scrollHeight; });
+loadMessages().then((len) => {
+  if (len > 0) {
+    chatEl.scrollTop = chatEl.scrollHeight;
+  }
+  // start polling for new messages
+  setTimeout(pollForMessages, pollInterval);
+});
