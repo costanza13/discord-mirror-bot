ef85ddfa19b8bed8d6d626b89a18dfa7a9c7cc16
mcc <costanza+git@gmail.com>
2021-09-29 17:28:37 -0700

    support mulitple allowed origins for CORS

--- server.js
+++ server.js
@@ -23,13 +23,44 @@ app.use(function (req, res) {
   res.end('Not found.');
 });
 
+let allowedOrigins;
+if (process.env.ORIGINS) {
+  allowedOrigins = process.env.ORIGINS.split(',');
+} else if (process.env.ORIGIN) {
+  allowedOrigins = [process.env.ORIGIN];
+} else {
+  allowedOrigins = [`http://localhost:${PORT}`];
+}
+
 const server = require('http').createServer(app);
 app.locals.io = require('socket.io')(server, {
   cors: {
-    origin: process.env.ORIGIN || `http://localhost:${PORT}`,
+    origin: function (origin, callback) {
+      // bypass the requests with no origin (like curl requests, mobile apps, etc )
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.indexOf(origin) === -1) {
+        var msg = `This site ${origin} does not have an access. Only specific domains are allowed to access it.`;
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
     methods: ['GET'],
   }
 });
+
+// app.use(cors({
+//   origin: function (origin, callback) {
+//     // bypass the requests with no origin (like curl requests, mobile apps, etc )
+//     if (!origin) return callback(null, true);
+
+//     if (allowedDomains.indexOf(origin) === -1) {
+//       var msg = `This site ${origin} does not have an access. Only specific domains are allowed to access it.`;
+//       return callback(new Error(msg), false);
+//     }
+//     return callback(null, true);
+//   }
+// }));
 
 botInit(app.locals);
 
